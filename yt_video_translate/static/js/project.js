2cf407ccfa46c90707d0e07bb117b2ca6f667e5b
Rajdeep Mondal <sachinrajdeeptendulkar@gmail.com>
2021-04-17 14:56:50 +0530

    trying onsumbit

--- yt_video_translate/static/js/project.js
+++ yt_video_translate/static/js/project.js
@@ -7,20 +7,22 @@
 //     });
 // });
 
-const url = `${location.pathname}status/`;
-const redirectURL = `${location.pathname}translated`;
-const checkStatus = () => {
-  console.log("Checking article status.");
-  fetch(url).then((resp) => {
-    console.log("Got article status:", resp.status);
-    if (resp.status == 200) {
-      console.log("Article ready - refresh page.");
-      //   location.reload();
-      location.assign(redirectURL);
-    } else {
-      console.log("Article processing - wait.");
-    }
-  });
+document.getElementById("input_form").onsubmit = function () {
+  const url = `${location.pathname}status/`;
+  const redirectURL = `${location.pathname}translated`;
+  const checkStatus = () => {
+    console.log("Checking article status.");
+    fetch(url).then((resp) => {
+      console.log("Got article status:", resp.status);
+      if (resp.status == 200) {
+        console.log("Article ready - refresh page.");
+        //   location.reload();
+        location.assign(redirectURL);
+      } else {
+        console.log("Article processing - wait.");
+      }
+    });
+  };
+  checkStatus();
+  setInterval(checkStatus, 2000);
 };
-checkStatus();
-setInterval(checkStatus, 2000);
