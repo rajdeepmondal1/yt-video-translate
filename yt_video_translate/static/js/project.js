995061c05455b88090a5712b95884c133aba9210
Rajdeep Mondal <sachinrajdeeptendulkar@gmail.com>
2021-04-19 23:12:13 +0530

    a little revamp

--- yt_video_translate/static/js/project.js
+++ yt_video_translate/static/js/project.js
@@ -26,3 +26,25 @@ const checkStatus = () => {
 checkStatus();
 setInterval(checkStatus, 2000);
 // };
+
+var start = document.getElementById("translate_button"),
+  current_progress = 0,
+  step = 0.5; // the smaller this is the slower the progress bar
+
+start.onclick = function () {
+  interval = setInterval(function () {
+    current_progress += step;
+    progress =
+      Math.round((Math.atan(current_progress) / (Math.PI / 2)) * 100 * 1000) /
+      1000;
+    $(".progress-bar")
+      .css("width", progress + "%")
+      .attr("aria-valuenow", progress)
+      .text(progress + "%");
+    if (progress >= 100) {
+      clearInterval(interval);
+    } else if (progress >= 70) {
+      step = 0.1;
+    }
+  }, 100);
+};
