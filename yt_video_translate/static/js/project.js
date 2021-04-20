c3a1d7bbeba472131ae154c5bade725886457c44
Rajdeep Mondal <sachinrajdeeptendulkar@gmail.com>
2021-04-20 18:39:30 +0530

    onclick

--- yt_video_translate/static/js/project.js
+++ yt_video_translate/static/js/project.js
@@ -50,3 +50,28 @@ setInterval(checkStatus, 2000);
 //     }
 //   }, 100);
 // };
+
+var start = document.getElementById("start"),
+  current_progress = 0,
+  step = 0.5; // the smaller this is the slower the progress bar
+
+window.onload = function () {
+  start.click();
+  start.onclick = function () {
+    interval = setInterval(function () {
+      current_progress += step;
+      progress =
+        Math.round((Math.atan(current_progress) / (Math.PI / 2)) * 100 * 1000) /
+        1000;
+      $(".progress-bar")
+        .css("width", progress + "%")
+        .attr("aria-valuenow", progress)
+        .text(progress + "%");
+      if (progress >= 100) {
+        clearInterval(interval);
+      } else if (progress >= 70) {
+        step = 0.1;
+      }
+    }, 100);
+  };
+};
