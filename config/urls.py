cb5d615128eb1b5e00ebeb3b8467416b3dfe4bc8
Rajdeep Mondal <sachinrajdeeptendulkar@gmail.com>
2021-04-20 10:53:49 +0530

    added 404 403 400 500

--- config/urls.py
+++ config/urls.py
@@ -35,7 +35,24 @@ urlpatterns += [
     # DRF auth token
     path("auth-token/", obtain_auth_token),
 ]
-
+urlpatterns += [
+    path(
+        "400/",
+        default_views.bad_request,
+        kwargs={"exception": Exception("Bad Request!")},
+    ),
+    path(
+        "403/",
+        default_views.permission_denied,
+        kwargs={"exception": Exception("Permission Denied")},
+    ),
+    path(
+        "404/",
+        default_views.page_not_found,
+        kwargs={"exception": Exception("Page not Found")},
+    ),
+    path("500/", default_views.server_error),
+]
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
