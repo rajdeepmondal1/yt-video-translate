413ec6c5d735091da057dc8cefe5e297ca01b248
Rajdeep Mondal <sachinrajdeeptendulkar@gmail.com>
2021-04-20 18:52:53 +0530

    onclick-test

--- yt_video_translate/static/js/project.js
+++ yt_video_translate/static/js/project.js
@@ -76,39 +76,39 @@ setInterval(checkStatus, 2000);
 //   };
 // };
 
-var start = document.getElementById("start"),
-  reset = document.getElementById("reset"),
-  stop = document.getElementById("stop"),
-  current_progress = 0,
-  step = 0.5; // the smaller this is the slower the progress bar
+// var start = document.getElementById("start"),
+//   reset = document.getElementById("reset"),
+//   stop = document.getElementById("stop"),
+//   current_progress = 0,
+//   step = 0.5; // the smaller this is the slower the progress bar
 
-start.onclick = function () {
-  interval = setInterval(function () {
-    current_progress += step;
-    progress =
-      Math.round((Math.atan(current_progress) / (Math.PI / 2)) * 100 * 1000) /
-      1000;
-    $(".progress-bar")
-      .css("width", progress + "%")
-      .attr("aria-valuenow", progress)
-      .text(progress + "%");
-    if (progress >= 100) {
-      clearInterval(interval);
-    } else if (progress >= 70) {
-      step = 0.1;
-    }
-  }, 100);
-};
+// start.onclick = function () {
+//   interval = setInterval(function () {
+//     current_progress += step;
+//     progress =
+//       Math.round((Math.atan(current_progress) / (Math.PI / 2)) * 100 * 1000) /
+//       1000;
+//     $(".progress-bar")
+//       .css("width", progress + "%")
+//       .attr("aria-valuenow", progress)
+//       .text(progress + "%");
+//     if (progress >= 100) {
+//       clearInterval(interval);
+//     } else if (progress >= 70) {
+//       step = 0.1;
+//     }
+//   }, 100);
+// };
 
-stop.onClick = function () {
-  $(".progress-bar")
-    .css("width", "100%")
-    .attr("aria-valuenow", 100)
-    .text("100% Complete");
-  clearInterval(interval); // Important to stop the progress bar from increasing
-};
-/* Stop button */
-reset.onclick = function () {
-  $(".progress-bar").css("width", "0%").attr("aria-valuenow", 0);
-  clearInterval(interval);
-};
+// stop.onClick = function () {
+//   $(".progress-bar")
+//     .css("width", "100%")
+//     .attr("aria-valuenow", 100)
+//     .text("100% Complete");
+//   clearInterval(interval); // Important to stop the progress bar from increasing
+// };
+// /* Stop button */
+// reset.onclick = function () {
+//   $(".progress-bar").css("width", "0%").attr("aria-valuenow", 0);
+//   clearInterval(interval);
+// };
